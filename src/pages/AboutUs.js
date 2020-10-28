aa22f257e8bb95b5678030b38832fe8c4ff4c947
ogubuike245 <ogubuike245@users.noreply.github.com>
2020-10-28 20:49:59 +0300

    for tonight

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,21 +1,31 @@
 import React from "react";
-import Hero from "../components/Hero";
-import Banner from "../components/Banner";
+import about from "../images/bg/bg.jpg";
 import { Link } from "react-router-dom";
 
 import Footer from "../components/Footer";
 
+import Title from "../components/Title";
+import { Button } from "../components/Button/Button";
+
 function AboutUs() {
   return (
     <>
-      <Hero>
-        <Banner title="ABOUT US" subtitle="">
-          <Link to="/rooms" className="btn-primary">
-            OUR ROOMS
-          </Link>
-        </Banner>
-      </Hero>
-
+      <section className="about">
+        <Title title="ABOUT US" span="serviceColor" />
+        <div className="abouty">
+          <div className="image">
+            <img src={about} alt="about" width="400px" height="400px" />
+          </div>
+          <div className="text">
+            <p>
+              <span>BASIC HOMES UG...</span> Bond a dweller directly to a
+              landlord in a single click as well as promoting the skills of
+              students to those who demand for them to help the students earn
+              Money.
+            </p>
+          </div>
+        </div>
+      </section>
       <Footer />
     </>
   );
