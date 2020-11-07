f43ef5c8c6a9d24abf8e37c010f600f01e2a60c7
ogubuike245 <ogubuike245@users.noreply.github.com>
2020-11-07 03:34:05 +0300

    Update RegisterSkill.js

--- src/components/ServiceCenter/RegisterSkill.js
+++ src/components/ServiceCenter/RegisterSkill.js
@@ -22,12 +22,7 @@ const RegisterSkill = () => {
 
         <div className="showcase-form card">
           <h2>REGISTER YOUR SKILL</h2>
-          <form
-            method="POST"
-            name="contact"
-            action="/contact"
-            data-netlify="true"
-          >
+          <form method="POST" name="contact" data-netlify="true">
             <input type="hidden" name="form-name" value="contact" />
             <div className="form-control">
               <label htmlFor="name">NAME</label>
