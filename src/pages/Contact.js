b3a084a38ca04f917f45414c1e613ee1340022ea
ogubuike245 <ogubuike245@users.noreply.github.com>
2020-11-09 01:49:02 +0300

    CONTACT PAGE EDIT

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,20 +1,70 @@
 import React from "react";
 import Hero from "../components/dashboard/Hero";
 import Banner from "../components/dashboard/Banner";
-import { Link } from "react-router-dom";
+
 import Footer from "../components/dashboard/Footer";
 import "../css/Contact.css";
+import { Button } from "../components/Button/Button";
+import { FaFacebookF, FaInstagram, FaMailBulk } from "react-icons/fa";
+import Title from "../components/Extras/Title";
 
 export default function Contact() {
   return (
     <>
       <Hero hero="roomsHero">
-        <Banner title="CONTACT US" subtitle="">
-          <Link to="/rooms" className="btn-primary">
-            OUR ROOMS
-          </Link>
-        </Banner>
+        <Banner title="CONTACT US" subtitle=""></Banner>
       </Hero>
+      <div className="contact-container">
+        <div className="contact-card">
+          <div className="contact-box">
+            <div className="contact-content">
+              <h2>01</h2>
+              <h3>CONTACT US VIA EMAIL</h3>
+              <p>BASIC HOMES UGANDA</p>
+              <Button
+                buttonStyle="btn--outline "
+                buttonSize="btn--mobile"
+                buttonColor="red"
+              >
+                <FaMailBulk />
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="contact-card">
+          <div className="contact-box">
+            <div className="contact-content">
+              <h2>02</h2>
+              <h3>FIND US ON INSTAGRAM</h3>
+              <p>BASIC HOMES UGANDA</p>
+              <Button
+                buttonStyle="btn--outline "
+                buttonSize="btn--mobile"
+                buttonColor="red"
+              >
+                <FaInstagram style={{ color: "#000000" }} />
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="contact-card">
+          <div className="contact-box">
+            <div className="contact-content">
+              <h2>03</h2>
+              <h3>FIND US ON FACEBOOK</h3>
+              <p>BASIC HOMES UGANDA</p>
+              <Button
+                buttonStyle="btn--outline "
+                buttonSize="btn--mobile"
+                buttonColor="red"
+              >
+                <FaFacebookF style={{ color: "#000000" }} />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Title title="LOCATION" />
       <Footer />
     </>
   );
